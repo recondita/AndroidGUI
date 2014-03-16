2480267e08fcc728cd4d310b7e3c1e6265d487a9
Jan <jan.hofmeier@yahoo.de>
2014-03-16 20:05:07 +0100

    Klasse

---
+++ src/AndroidSchnittstelle.java
@@ -0,0 +1,42 @@
+import spiel.Schnittstelle;
+
+
+public class AndroidSchnittstelle extends Schnittstelle
+{
+
+	@Override
+	public int figurMenu()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void meldungAusgeben(String meldung)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aktualisieren(int x, int y)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aktualisieren()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nachricht(String s)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
